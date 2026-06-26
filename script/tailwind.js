d8b19861560473c562ab2e1df838b5dc5e07b5e2
Idris Haidir <idris.haidir1@gmail.com>
2026-06-26 23:22:59 +0700

    first commit

---
+++ script/tailwind.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          red: "#E3000F", // IndiHome / Telkom Red
+          dark: "#1a1a1a",
+          light: "#f8fafc",
+        },
+      },
+      fontFamily: {
+        sans: [
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "sans-serif",
+        ],
+      },
+    },
+  },
+};
